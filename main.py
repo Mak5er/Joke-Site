c56d9b1607f485df1c241f48f65d83e916e36df0
Mak5er <mmaksym.reva@gmail.com>
2023-08-08 14:05:34 +0300

    add 1sec delay to getting a new joke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, jsonify
-import sqlite3
 import random
+import sqlite3
+import time
+
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -11,11 +13,10 @@ background_color = "#212937"
 
 # Функція для отримання рандомного анекдоту з бази даних
 def get_random_joke():
-    conn = sqlite3.connect('jokes.db')
-    cursor = conn.cursor()
-    cursor.execute('SELECT text FROM jokes_uk')
-    jokes = cursor.fetchall()
-    conn.close()
+    with sqlite3.connect('jokes.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT text FROM jokes_uk')
+        jokes = cursor.fetchall()
 
     if jokes:
         random_joke = random.choice(jokes)[0]
@@ -31,10 +32,19 @@ def index():
                            background_color=background_color)
 
 
+last_request_time = 0
+
 @app.route('/get_random_joke', methods=['POST'])
 def get_random_joke_ajax():
-    random_joke = get_random_joke()
-    return jsonify(random_joke)
+    global last_request_time
+    current_time = time.time()
+
+    if current_time - last_request_time >= 1: # Перевірка, чи пройшла 1 секунда з останнього запиту
+        last_request_time = current_time # Оновлюємо час останнього запиту
+        random_joke = get_random_joke()
+        return jsonify(random_joke)
+    else:
+        pass
 
 
 if __name__ == '__main__':
